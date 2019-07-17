775bc9ed5c986f97671f394454f5f9466f8cf1a2
Sunghyun Kim <sunghyun_k@icloud.com>
2019-07-17 11:56:34 +0900

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by 김성현 on 17/07/2019.
+//  Copyright © 2019 Sunghyun Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
